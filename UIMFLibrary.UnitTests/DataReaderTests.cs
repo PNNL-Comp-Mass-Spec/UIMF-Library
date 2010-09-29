70ac56a5d4b048ee4013c63593af95bcfc055f58
Gordon Slysz <gordon.slysz@pnnl.gov>
2010-09-29 18:01:28 +0000

    added DataReaderTests
    git-svn-id: http://prismsvn.pnl.gov/svn/Infrastructure_Tools/IMS/UIMFLibrary/trunk@7560 91f24f89-0e13-0410-88a5-a0bb1538c76e

---
+++ UIMFLibrary.UnitTests/DataReaderTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NUnit.Framework;
+
+namespace UIMFLibrary.UnitTests
+{
+    [TestFixture]
+    public class DataReaderTests
+    {
+        [Test]
+        public void readMSLevelDataFromFileContainingBothMS_and_MSMSData()
+        {
+
+            string filePath = @"\\protoapps\UserData\Slysz\DeconTools_TestFiles\QC_Shew_MSMS_500_100_fr1200_c2_Ek_0000.uimf";
+
+            UIMFLibrary.DataReader reader = new DataReader();
+            reader.OpenUIMF(filePath);
+
+            GlobalParameters gp = reader.GetGlobalParameters();
+
+            int numBins = gp.Bins;
+
+            double[] xvals = new double[numBins];
+            int[] yvals = new int[numBins];
+
+
+            reader.SumScansRange(xvals, yvals, 1, 9, 2, 100, 500);
+
+            Assert.AreNotEqual(null, xvals);
+            Assert.AreNotEqual(0, xvals.Length);
+
+
+
+
+        }
+
+
+    }
+}
