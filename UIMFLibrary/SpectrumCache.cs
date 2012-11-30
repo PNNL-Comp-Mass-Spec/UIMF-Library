7be906d6f6db4e644319cae89a3bdf20ac9237e1
Kevin Crowell <Kevin.Crowell@pnnl.gov>
2012-11-30 23:27:52 +0000

    - Added missing SpectrumCache class
    git-svn-id: http://prismsvn.pnl.gov/svn/Infrastructure_Tools/IMS/UIMFLibrary/trunk@12909 91f24f89-0e13-0410-88a5-a0bb1538c76e

---
+++ UIMFLibrary/SpectrumCache.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UIMFLibrary
+{
+	public class SpectrumCache
+	{
+		public int StartFrameNumber { get; private set; }
+		public int EndFrameNumber { get; private set; }
+		public IList<IDictionary<int, int>> ListOfIntensityDictionries { get; private set; }
+
+		public SpectrumCache(int startFrameNumber, int endFrameNumber, IList<IDictionary<int, int>> listOfIntensityDictionaries)
+		{
+			this.StartFrameNumber = startFrameNumber;
+			this.EndFrameNumber = endFrameNumber;
+			this.ListOfIntensityDictionries = listOfIntensityDictionaries;
+		}
+	}
+}
