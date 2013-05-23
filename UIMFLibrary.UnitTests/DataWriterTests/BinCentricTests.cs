50ad178138f063b1803d902964f32fd2f08da39d
Kevin Crowell <Kevin.Crowell@pnnl.gov>
2013-05-23 17:29:14 +0000

    - Added bin centric tests
    git-svn-id: http://prismsvn.pnl.gov/svn/Infrastructure_Tools/IMS/UIMFLibrary/trunk@14444 91f24f89-0e13-0410-88a5-a0bb1538c76e

---
+++ UIMFLibrary.UnitTests/DataWriterTests/BinCentricTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace UIMFLibrary.UnitTests.DataWriterTests
+{
+	public class BinCentricTests
+	{
+		[Test]
+		public void TestCreateBinCentricTables()
+		{
+			string fileLocation = @"..\..\..\TestFiles\SarcCtrl_P21_1mgml_IMS6_AgTOF07_210min_CID_01_05Oct12_Frodo_Precursors_Removed_Collision_Energy_Collapsed.UIMF";
+			FileInfo uimfFile = new FileInfo(fileLocation);
+
+			DataWriter uimfWriter = new DataWriter();
+			uimfWriter.OpenUIMF(uimfFile.FullName);
+
+			uimfWriter.CreateBinCentricTables();
+
+			uimfWriter.CloseUIMF();
+		}
+
+		[Test]
+		public void TestCreateBinCentricTablesSmallFile()
+		{
+			string fileLocation = @"..\..\..\TestFiles\PepMix_MSMS_4msSA.UIMF";
+			FileInfo uimfFile = new FileInfo(fileLocation);
+
+			DataWriter uimfWriter = new DataWriter();
+			uimfWriter.OpenUIMF(uimfFile.FullName);
+
+			uimfWriter.CreateBinCentricTables();
+
+			uimfWriter.CloseUIMF();
+		}
+
+		[Test]
+		public void TestEncodeDecodeFunctionality()
+		{
+			int scanLc = 183;
+			int scanIms = 217;
+			int numImsScans = 360;
+
+			int calculatedIndex = (scanLc * numImsScans) + scanIms;
+
+			int calculatedScanLc = calculatedIndex / numImsScans;
+			int calculatedScanIms = calculatedIndex % numImsScans;
+
+			Assert.AreEqual(calculatedScanLc, scanLc);
+			Assert.AreEqual(calculatedScanIms, scanIms);
+		}
+	}
+}
