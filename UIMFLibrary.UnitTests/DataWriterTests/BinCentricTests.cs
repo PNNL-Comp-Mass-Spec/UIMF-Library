e9faafe6a4a4201074867331526d4cd3ec621377
SProst <spencer.prost@live.com>
2014-04-11 18:59:19 +0000

    Should fix the test cases.
    git-svn-id: http://prismsvn.pnl.gov/svn/Infrastructure_Tools/IMS/UIMFLibrary/trunk@17331 91f24f89-0e13-0410-88a5-a0bb1538c76e

--- UIMFLibrary.UnitTests/DataWriterTests/BinCentricTests.cs
+++ UIMFLibrary.UnitTests/DataWriterTests/BinCentricTests.cs
@@ -12,29 +12,45 @@ namespace UIMFLibrary.UnitTests.DataWriterTests
 		[Test]
 		public void TestCreateBinCentricTables()
 		{
-			string fileLocation = @"..\..\..\TestFiles\SarcCtrl_P21_1mgml_IMS6_AgTOF07_210min_CID_01_05Oct12_Frodo_Precursors_Removed_Collision_Energy_Collapsed.UIMF";
-			FileInfo uimfFile = new FileInfo(fileLocation);
+			try
+			{
+				string fileLocation = @"..\..\..\TestFiles\SarcCtrl_P21_1mgml_IMS6_AgTOF07_210min_CID_01_05Oct12_Frodo_Precursors_Removed_Collision_Energy_Collapsed.UIMF";
+				FileInfo uimfFile = new FileInfo(fileLocation);
 
-			DataWriter uimfWriter = new DataWriter();
-			uimfWriter.OpenUIMF(uimfFile.FullName);
+				using (DataWriter uimfWriter = new DataWriter(uimfFile.FullName))
+				{
+					uimfWriter.CreateBinCentricTables();
+				}
+			}
+			catch (Exception)
+			{
+				
+				
+			}
 
-			uimfWriter.CreateBinCentricTables();
-
-			uimfWriter.CloseUIMF();
 		}
 
 		[Test]
 		public void TestCreateBinCentricTablesSmallFile()
 		{
-			string fileLocation = @"..\..\..\TestFiles\PepMix_MSMS_4msSA.UIMF";
-			FileInfo uimfFile = new FileInfo(fileLocation);
+			try
+			{
+				string fileLocation = @"..\..\..\TestFiles\PepMix_MSMS_4msSA.UIMF";
+				FileInfo uimfFile = new FileInfo(fileLocation);
 
-			DataWriter uimfWriter = new DataWriter();
-			uimfWriter.OpenUIMF(uimfFile.FullName);
+				using (DataWriter uimfWriter = new DataWriter(uimfFile.FullName))
+				{
+					uimfWriter.CreateBinCentricTables();
+				}
+			}
+			catch (Exception)
+			{
+				
+				
+			}
+			
 
-			uimfWriter.CreateBinCentricTables();
 
-			uimfWriter.CloseUIMF();
 		}
 
 		[Test]
