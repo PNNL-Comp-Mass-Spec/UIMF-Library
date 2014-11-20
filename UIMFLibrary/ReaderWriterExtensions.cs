9ffbedc371ec721937de8015f42f2ee170cb6e1e
alchemistmatt <matthew.monroe@pnnl.gov>
2014-11-19 21:22:20 -0800

    Add Async project (compiles under .NET 4.5.1)

---
+++ UIMFLibrary/ReaderWriterExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIMFLibrary
+{
+    /// <summary>
+    /// Extension methods that use functionality specific to .NET 4.5 and above
+    /// This class is included when compiling for .NET 4.5 but not included when compiling for .NET 4.0
+    /// </summary>
+    public static class ReaderWriterExtensions
+    {
+        public static async Task InsertFrameAsync(this DataWriter dataWriter, int frameNum, Dictionary<FrameParamKeyType, string> frameParameters)
+        {
+            try
+            {
+                await Task.Run(() => dataWriter.InsertFrame(frameNum, frameParameters));
+            }
+            catch (Exception ex)
+            {
+                ReportError("InsertFrame error: " + ex.Message, ex);
+            }
+        }
+
+        private static void ReportError(string errorMessage, Exception ex)
+        {
+            Console.WriteLine(errorMessage);
+            throw new Exception(errorMessage, ex);
+        }
+    }
+}
