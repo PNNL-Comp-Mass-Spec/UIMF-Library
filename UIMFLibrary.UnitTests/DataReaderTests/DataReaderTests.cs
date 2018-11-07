d4e80b9b5902a62234028b1ead616fc1703b1825
alchemistmatt <matthew.monroe@pnnl.gov>
2018-11-06 18:20:49 -0800

    Remove old code and check for null

--- UIMFLibrary.UnitTests/DataReaderTests/DataReaderTests.cs
+++ UIMFLibrary.UnitTests/DataReaderTests/DataReaderTests.cs
@@ -1122,163 +1122,6 @@ namespace UIMFLibrary.UnitTests.DataReaderTests
 
         #endregion
 
-        ////TODO: this test fails... not sure we need it.
-        // [Test]
-        // public void GetBPISortedList()
-        // {
-        // PrintMethodName(System.Reflection.MethodBase.GetCurrentMethod());
-        // string filePath = @"\\proto-2\UnitTest_Files\DeconTools_TestFiles\UIMF\Sarc_MS_90_21Aug10_Cheetah_10-08-02_0000.uimf";
-
-        // m_reader = new DataReader();
-        // m_reader.OpenUIMF(filePath);
-
-        // GlobalParameters gp = m_reader.GetGlobalParameters();
-        // FrameParameters fp = m_reader.GetFrameParameters(1);
-
-        // double[] bpi = new double[gp.NumFrames * fp.Scans];
-
-        // int startFrame = 500;
-        // int stopFrame = 800;
-
-        // m_reader.GetBPI(bpi, 0, startFrame, stopFrame, 0, 600);
-
-        // m_reader.CloseUIMF();
-        // }
-
-        ////TODO: this takes a long time.  Not sure we need it
-        // [Test]
-        // public void getBPIListTest()
-        // {
-        // PrintMethodName(System.Reflection.MethodBase.GetCurrentMethod());
-        // UIMFLibrary.DataReader reader = new DataReader();
-        // reader.OpenUIMF(FileRefs.uimfStandardFile1);
-
-        // Stack<int[]> bpiStack = reader.GetFrameAndScanListByDescendingIntensity();
-
-        // Console.WriteLine("The list is " + bpiStack.Count.ToString());
-        // reader.CloseUIMF();
-
-        // }
-
-        // TODO:  test seems to write out mostly zeros....  we should test a region richer in intensity data
-        // TODO:  is this method the same as another??  Check against Get3DProfile
-
-        // TODO:  this test fails on Gord's machine..... ok on Hudson??   Need to resolve this
-        // [Test]
-        // public void variableSummingTest()
-        // {
-        // PrintMethodName(System.Reflection.MethodBase.GetCurrentMethod());
-        // UIMFLibrary.DataReader reader = new DataReader();
-        // reader.OpenUIMF(FileRefs.uimfStandardFile1);
-
-        // GlobalParameters gp = reader.GetGlobalParameters();
-
-        // int numBins = gp.Bins;
-        // double[] xvals = new double[numBins];
-        // int[] yvals = new int[numBins];
-        // int[] yvals1 = new int[numBins];
-
-        // int endFrame = 564;
-        // int startFrame = 484;
-        // int startScan = 73;
-        // int endScan = 193;
-
-        // //sum a fixed range of scans within a set of frames
-        // reader.SumScans(xvals, yvals, 0, startFrame, endFrame, startScan, endScan);
-        // //reader.GetSpectrum(10, 350, yvals, yvals1);
-
-        // Console.WriteLine("Finished running sum scans");
-
-        // List<int> frameNumbers = new List<int>();
-        // //create a list of frame Numbers
-        // for (int i = 0; i < endFrame - startFrame + 1; i++)
-        // {
-        // frameNumbers.Add(i + startFrame);
-        // }
-
-        // List<List<int>> scanNumbersForEachFrame = new List<List<int>>();
-
-        // //create a single list of scan numbers for this test
-        // List<int> scanNumbers = new List<int>();
-
-        // for (int i = 0; i < endScan - startScan + 1; i++)
-        // {
-        // scanNumbers.Add(i + startScan);
-        // }
-
-        // for (int i = 0; i < endFrame - startFrame + 1; i++)
-        // {
-
-        // scanNumbersForEachFrame.Add(scanNumbers);
-
-        // }
-
-        // List<double> mzList = new List<double>();
-        // List<int> intensityList = new List<int>();
-
-        // reader.SumScansNonCached(frameNumbers, scanNumbersForEachFrame, mzList, intensityList, 0, 5000);
-        // //reader.SumScansForVariableRange(frameNumbers, scanNumbersForEachFrame, 0, yvals1);
-        // //Assert.AreEqual(yvals, yvals1);
-        // reader.CloseUIMF();
-
-        // }
-
-        // TODO:  fix paths;  move this test somewhere else
-        // [Test]
-        // public void IMSConverterTest_WriteFileTest1()
-        // {
-        // PrintMethodName(System.Reflection.MethodBase.GetCurrentMethod());
-        // string filePath = "C:\\proteomicssoftwaretools\\Imf2uimf\\IMSConverterTestfile\\8pepMix_200nM_0001.uimf";
-        // UIMFLibrary.DataReader reader = null;
-
-        // try
-        // {
-        // reader = new DataReader();
-        // reader.OpenUIMF(filePath);
-
-        // WriteFile(reader.getFileBytesFromTable("AcquireLogFile"), "C:\\proteomicssoftwaretools\\imf2uimf\\IMSConverterTestFile\\AcquireLog.txt");
-
-        // }
-        // finally
-        // {
-        // if (reader != null)
-        // {
-        // reader.CloseUIMF();
-        // }
-        // }
-        // }
-
-        // TODO: update this with a standard UIMF file
-        // [Test]
-        // public void getSpectrumTest()
-        // {
-        // PrintMethodName(System.Reflection.MethodBase.GetCurrentMethod());
-        // string filePath = @"\\proto-10\IMS_TOF2_DMS1\Dey_KO_8721_02_17Nov10_10-09-23_0000\Dey_KO_8721_02_17Nov10_10-09-23_0000.UIMF";
-        // UIMFLibrary.DataReader reader = new UIMFLibrary.DataReader();
-        // reader.OpenUIMF(filePath);
-
-        // GlobalParameters gp = reader.GetGlobalParameters();
-        // int numBins = gp.Bins;
-
-        // double[] xvals = new double[numBins];
-        // int[] yvals = new int[numBins];
-
-        // //         reader.SumScansNonCached(xvals, yvals, 0, 6, 6, 285, 285);
-
-        // reader.GetSpectrum(6, 285, yvals, xvals);
-
-        // StringBuilder sb = new StringBuilder();
-        // for (int i = 0; i < xvals.Length; i++)
-        // {
-        // sb.Append(xvals[i] + "\t" + yvals[i]);
-        // sb.Append(Environment.NewLine);
-        // }
-
-        // //Console.WriteLine(sb.ToString());
-
-        // reader.CloseUIMF();
-
-        // }
         #region Methods
 
         /// <summary>
@@ -1347,6 +1190,13 @@ namespace UIMFLibrary.UnitTests.DataReaderTests
             {
                 relativePathsToCheck.Add(relativeFilePath);
             }
+
+            if (uimfFile.Directory == null)
+            {
+                Assert.Fail("Could not find determine the parent directory of " + uimfFile.FullName);
+                return null;
+            }
+
             var parentToCheck = uimfFile.Directory.Parent;
             while (parentToCheck != null)
             {
