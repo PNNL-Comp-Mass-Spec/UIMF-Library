c6c355b9e46b9f188a542efe673caa614793c501
alchemistmatt <matthew.monroe@pnnl.gov>
2021-06-07 13:24:17 -0700

    Suppress warnings

--- UIMFLibrary/GlobalSuppressions.cs
+++ UIMFLibrary/GlobalSuppressions.cs
@@ -1,10 +1,23 @@
 ﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
+// Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:UIMFLibrary.FrameParameters.voltEntranceIFTOut")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:UIMFLibrary.FrameParameters.voltEntranceIFTIn")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:UIMFLibrary.FrameParameters.voltExitIFTIn")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:UIMFLibrary.FrameParameters.voltExitIFTOut")]
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:UIMFLibrary.BinCentricTableCreation.CreateIndexes(System.String,System.Int32)")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:UIMFLibrary.BinCentricTableCreation.CreateTemporaryDatabase(UIMFLibrary.DataReader,System.String)~System.String")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:UIMFLibrary.BinCentricTableCreation.InsertBinCentricData(System.Data.SQLite.SQLiteConnection,System.Data.SQLite.SQLiteConnection,UIMFLibrary.DataReader)")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:UIMFLibrary.BinCentricTableCreation.SortDataForBin(System.Data.SQLite.SQLiteConnection,System.Data.SQLite.SQLiteCommand,System.Int32,System.Int32)")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:UIMFLibrary.CLZF2.lzf_compress(System.Byte[],System.Byte[]@,System.Int32)~System.Int32")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:UIMFLibrary.DataReader.AccumulateFrameData(System.Int32,System.Int32,System.Boolean,System.Int32,System.Int32,System.Int32,System.Int32,System.Double,System.Double)~System.Double[,]")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:UIMFLibrary.MzCalibrator.MZtoTOF(System.Double)~System.Int32")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:UIMFLibrary.UIMFData.ConvertBinToMZ(System.Double,System.Double,System.Double,System.Double,System.Int32)~System.Double")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:UIMFLibrary.UIMFData.ConvertBinToMz(System.Int32,System.Double,UIMFLibrary.FrameParameters)~System.Double")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:UIMFLibrary.UIMFData.ConvertBinToMz(System.Int32,System.Double,UIMFLibrary.FrameParams)~System.Double")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:UIMFLibrary.UIMFData.GetBinClosestToMZ(System.Double,System.Double,System.Double,System.Double,System.Double)~System.Double")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed name", Scope = "member", Target = "~P:UIMFLibrary.FrameParameters.voltEntranceIFTIn")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed name", Scope = "member", Target = "~P:UIMFLibrary.FrameParameters.voltEntranceIFTOut")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed name", Scope = "member", Target = "~P:UIMFLibrary.FrameParameters.voltExitIFTIn")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed name", Scope = "member", Target = "~P:UIMFLibrary.FrameParameters.voltExitIFTOut")]
