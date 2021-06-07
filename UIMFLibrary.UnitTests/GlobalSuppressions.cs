f6744ddb3134102e8191d719add47cb99fe37448
alchemistmatt <matthew.monroe@pnnl.gov>
2021-06-07 13:48:29 -0700

    Suppress warnings

---
+++ UIMFLibrary.UnitTests/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:UIMFLibrary.UnitTests.DataReaderTests.DataReaderTests.ConvertBinToMZ(System.Double,System.Double,System.Double,System.Double,System.Int32)~System.Double")]
+[assembly: SuppressMessage("Usage", "RCS1146:Use conditional access.", Justification = "Leave as-is for readability", Scope = "member", Target = "~M:UIMFLibrary.UnitTests.DataReaderTests.DataReaderTests.PrintMethodName(System.Reflection.MethodBase)")]
